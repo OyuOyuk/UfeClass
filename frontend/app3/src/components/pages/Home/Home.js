39b88c1e6bbf81cac67f19bacca2466a6e138a99
unknown <oyuruby412@gmail.com>
2026-03-19 15:35:14 +0800

    before jest

--- frontend/app3/src/components/pages/Home/Home.js
+++ frontend/app3/src/components/pages/Home/Home.js
@@ -1,9 +1,41 @@
+import { useState, useEffect } from "react";
+
+
 function Home() {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products")
+      .then(res => res.json())
+      .then(data => {
+        setProducts(data);
+        setLoading(false);
+      });
+  }, []); 
+
+  if (loading) return <p>Loading...</p>;
+
   return (
-    <div style={{ padding: 20 }}>
-      <h2>Home View</h2>
-      <p>Does this work</p>
-    </div>
+    <div >
+      <table>
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Price</th>
+            <th>Description</th>
+          </tr>
+        </thead>
+        {products.map(product => (
+          <tr>
+            <td>{product.title}</td>
+            <td>{product.price}</td>
+            <td>{product.description}</td>
+          </tr>
+        ))}
+      </table>
+        
+      </div>
   );
 }
 export default Home
--- frontend/app3/src/components/pages/Profile/Profile.js
+++ frontend/app3/src/components/pages/Profile/Profile.js
@@ -1,7 +1,7 @@
 import "./Profile.css"
 import { Navigate } from "react-router"
 function Profile({userinfo}){
-    if(userinfo == ''){
+    if(userinfo === ''){
         return(
             <Navigate to ="/login" replace/>
         )
